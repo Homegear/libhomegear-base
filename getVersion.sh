46d9bffe6f101ff9c7f350ce4892179741806f0d
Sathya Laufer <sathya@laufers.net>
2016-01-08 19:36:33 +0100

    HomeMatic BidCoS roaming now also changes the interface when the old one gets unavailable

--- getVersion.sh
+++ getVersion.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
-revision=0
+revision1=0
 wgetCount=0
-while [ $revision -eq 0 ] && [ $wgetCount -le 5 ]; do
+while [ $revision1 -eq 0 ] && [ $wgetCount -le 5 ]; do
 	rm -f contributors*
 	wget -q https://api.github.com/repos/Homegear/Homegear/stats/contributors
 	lines=`grep -Po '"total":.*[0-9]' contributors`
 	for l in $lines; do
 		    if [ "$l" != "\"total\":" ]; then
-		            revision=$(($revision + $l))
+		            revision1=$(($revision1 + $l))
 		    fi
 	done
 	wgetCount=$(($wgetCount + 1))
+	sleep 1
 done
 rm -f contributors*
+
+revision2=0
+wgetCount=0
+while [ $revision2 -eq 0 ] && [ $wgetCount -le 5 ]; do
+	rm -f contributors*
+	wget -q https://api.github.com/repos/Homegear/libhomegear-base/stats/contributors
+	lines=`grep -Po '"total":.*[0-9]' contributors`
+	for l in $lines; do
+		    if [ "$l" != "\"total\":" ]; then
+		            revision2=$(($revision2 + $l))
+		    fi
+	done
+	wgetCount=$(($wgetCount + 1))
+	sleep 1
+done
+rm -f contributors*
+
+revision=$(($revision1 + $revision2))
 echo 0.6.0-$revision
