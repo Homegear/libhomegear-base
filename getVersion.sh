2f064be7784b5ff815d4486304b0900d99cccf7d
Sathya Laufer <sathya@laufers.net>
2016-06-14 13:31:20 +0200

    New getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,35 +1,7 @@
 #!/bin/sh
-revision1=0
-wgetCount=0
-while [ $revision1 -eq 0 ] && [ $wgetCount -le 5 ]; do
-	rm -f contributors*
-	wget -q https://api.github.com/repos/Homegear/Homegear/stats/contributors
-	lines=`grep -Po '"total":.*[0-9]' contributors`
-	for l in $lines; do
-		    if [ "$l" != "\"total\":" ]; then
-		            revision1=$(($revision1 + $l))
-		    fi
-	done
-	wgetCount=$(($wgetCount + 1))
-	sleep 1
-done
-rm -f contributors*
 
-revision2=0
-wgetCount=0
-while [ $revision2 -eq 0 ] && [ $wgetCount -le 5 ]; do
-	rm -f contributors*
-	wget -q https://api.github.com/repos/Homegear/libhomegear-base/stats/contributors
-	lines=`grep -Po '"total":.*[0-9]' contributors`
-	for l in $lines; do
-		    if [ "$l" != "\"total\":" ]; then
-		            revision2=$(($revision2 + $l))
-		    fi
-	done
-	wgetCount=$(($wgetCount + 1))
-	sleep 1
-done
-rm -f contributors*
+SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 
-revision=$(($revision1 + $revision2))
-echo 0.7.0-$revision
+REVISION_FILE=$SCRIPTDIR/revision.txt
+REVISION=$(cat $REVISION_FILE)
+echo 0.7.0-$REVISION
