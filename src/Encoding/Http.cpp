8cdf45eb5cbf0612dd96f933cbb560225b4d93e3
Sathya Laufer <sathya@laufers.net>
2017-07-28 15:54:01 +0200

    Changes to HTTP client

--- src/Encoding/Http.cpp
+++ src/Encoding/Http.cpp
@@ -4,16 +4,16 @@
  * modify it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * libhomegear-base is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with libhomegear-base.  If not, see
  * <http://www.gnu.org/licenses/>.
- * 
+ *
  * In addition, as a special exception, the copyright holders give
  * permission to link the code of portions of this program with the
  * OpenSSL library under certain conditions as described in each
@@ -334,7 +334,6 @@ int32_t Http::processHeader(char** buffer, int32_t& bufferLength)
 	{
 		_type = Type::Enum::response;
 		_header.responseCode = strtol(headerBuffer + 9, NULL, 10);
-		if(_header.responseCode != 200) throw HttpException("Response code was: " + std::to_string(_header.responseCode), _header.responseCode);
 	}
 	else
 	{
