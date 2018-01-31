8a52443c3923c7eda5b32f4ce620c6f2470eca65
Sathya Laufer <sathya@laufers.net>
2018-01-31 22:38:49 +0100

    Cast CFM binary data to string

--- src/DeviceDescription/ParameterCast.cpp
+++ src/DeviceDescription/ParameterCast.cpp
@@ -864,6 +864,8 @@ CcrtdnParty::CcrtdnParty(BaseLib::SharedObjects* baseLib, xml_node<>* node, Para
 
 void CcrtdnParty::fromPacket(PVariable value)
 {
+	value->stringValue = "";
+	value->type = VariableType::tString;
 }
 
 void CcrtdnParty::toPacket(PVariable value)
