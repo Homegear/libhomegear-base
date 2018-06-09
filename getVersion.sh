d23652775a33d9ca72804449bbefced100723cc4
Sathya Laufer <sathya@laufers.net>
2018-06-09 13:06:07 +0200

    Correct getVersion.sh

--- getVersion.sh
+++ getVersion.sh
@@ -1,7 +1,36 @@
 #!/bin/sh
 
-SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
+revision1=0
+wgetCount=0
+while [ $revision1 -eq 0 ] && [ $wgetCount -le 5 ]; do
+	rm -f contributors*
+	wget -q https://api.github.com/repos/Homegear/Homegear/stats/contributors
+	lines=`grep -Po '"total":.*[0-9]' contributors`
+	for l in $lines; do
+		    if [ "$l" != "\"total\":" ]; then
+		            revision1=$(($revision1 + $l))
+		    fi
+	done
+	wgetCount=$(($wgetCount + 1))
+	sleep 1
+done
+rm -f contributors*
 
-REVISION_FILE=$SCRIPTDIR/revision.txt
-REVISION=$(cat $REVISION_FILE)
-echo 0.8.0-$REVISION
+revision2=0
+wgetCount=0
+while [ $revision2 -eq 0 ] && [ $wgetCount -le 5 ]; do
+	rm -f contributors*
+	wget -q https://api.github.com/repos/Homegear/libhomegear-base/stats/contributors
+	lines=`grep -Po '"total":.*[0-9]' contributors`
+	for l in $lines; do
+		    if [ "$l" != "\"total\":" ]; then
+		            revision2=$(($revision2 + $l))
+		    fi
+	done
+	wgetCount=$(($wgetCount + 1))
+	sleep 1
+done
+rm -f contributors*
+
+revision=$(($revision1 + $revision2))
+echo 0.7.20-$revision
