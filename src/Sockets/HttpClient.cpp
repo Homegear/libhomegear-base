6a9dbfd043f8578dd79e222ad0c923c9860de50c
Sathya Laufer <sathya@laufers.net>
2018-06-19 16:04:03 +0200

    Fixed error in HTTP chunked content processing

--- src/Sockets/HttpClient.cpp
+++ src/Sockets/HttpClient.cpp
@@ -213,6 +213,7 @@ void HttpClient::sendRequest(const std::string& request, Http& http, bool respon
 				_socketMutex.unlock();
 				throw HttpClientException("Unable to read from HTTP server \"" + _hostname + "\" (3): " + ex.what());
 			}
+
 			if(bufferPos + receivedBytes > bufferMax)
 			{
 				if(!_keepAlive) _socket->close();
