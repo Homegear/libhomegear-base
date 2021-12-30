28d5bfa21a970a8e0e7acb177c1ffb631502f592
Dr. Sathya Laufer <s.laufer@homegear.email>
2021-12-30 14:38:24 +0100

    Revision test

--- getVersion.sh
+++ getVersion.sh
@@ -4,4 +4,4 @@ SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 
 REVISION_FILE=$SCRIPTDIR/revision.txt
 REVISION=$(cat $REVISION_FILE)
-echo "0.8.0-$REVISION.$(date '+%Y%m%d%H%M%S')"
+echo "0.8.0-${REVISION}d$(date '+%Y%m%d%H%M%S')"
