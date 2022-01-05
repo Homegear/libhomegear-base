220547875b5e5f571713cfeeeb63acf3976125e5
Dr. Sathya Laufer <s.laufer@homegear.email>
2022-01-05 20:21:04 +0100

    Shortened revision

--- getVersion.sh
+++ getVersion.sh
@@ -4,4 +4,4 @@ SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 
 REVISION_FILE=$SCRIPTDIR/revision.txt
 REVISION=$(cat $REVISION_FILE)
-echo "0.8.$(date '+%Y%m%d%H%M%S')-${REVISION}"
+echo "0.8.$(date '+%y%m%d')-${REVISION}"
