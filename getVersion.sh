b68cf633c3eec1a8d1ec65fe3c56189ad18e078c
Dr. Sathya Laufer <s.laufer@homegear.email>
2022-01-18 00:08:26 +0100

    Added hour to version

--- getVersion.sh
+++ getVersion.sh
@@ -4,4 +4,4 @@ SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 
 REVISION_FILE=$SCRIPTDIR/revision.txt
 REVISION=$(cat $REVISION_FILE)
-echo "0.8.$(date '+%y%m%d')-${REVISION}"
+echo "0.8.$(date '+%y%m%d%H')-${REVISION}"
