4ef8cb84775f1e55ab707fd1a61e72b066788f89
Dr. Sathya Laufer <s.laufer@homegear.email>
2024-09-23 10:33:32 +0200

    Bumped version to 0.10

--- getVersion.sh
+++ getVersion.sh
@@ -4,4 +4,4 @@ SCRIPTDIR="$( cd "$(dirname $0)" && pwd )"
 
 REVISION_FILE=$SCRIPTDIR/revision.txt
 REVISION=$(cat $REVISION_FILE)
-echo "0.9.$(date '+%y%m%d%H')-${REVISION}"
+echo "0.10.$(date '+%y%m%d%H')-${REVISION}"
